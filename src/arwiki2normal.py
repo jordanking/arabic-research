71fc0d80161eedc27715f48adcd0a53f3a005e27
Jordan King <jwk67@georgetown.edu>
2015-09-29 17:49:27 -0400

    Updated to new arapy

--- src/arwiki2normal.py
+++ src/arwiki2normal.py
@@ -1,9 +1,12 @@
 import sys
+
+# add the path of arapy
 sys.path.insert(0,"/home/jordan/Documents/Projects/")
-from arapy.preprocessing import parse_wiki_dump
+from arapy.util import parse_wiki_dump
 
 """
-This code takes in and out args for a wiki xml dump in, and normalized arabic sentences out.
+This code takes in and out args for a wiki xml dump in, 
+and normalized arabic sentences out.
 """
 
 parse_wiki_dump(sys.argv[1], sys.argv[2], True)
