47ec5eefaed6d493011b57e43cd383e62969bd83
Jordan King <jwk67@georgetown.edu>
2015-11-01 16:05:37 -0500

    Began writing paper/Sim task 2

--- src/create_synonym_task.py
+++ src/create_synonym_task.py
@@ -12,6 +12,7 @@ import random
 sys.path.insert(0,"/Users/jordanking/Documents/")
 import arapy.thesaurus as thes
 import arapy.normalization as norm
+import arapy.madamira as mada
 
 # random seeding for reproducing
 random.seed(1)
@@ -20,14 +21,11 @@ random.seed(1)
 logging.basicConfig(format='%(asctime)s : %(levelname)s : %(message)s',level=logging.INFO)
 
 # file to extract most common from
-# sourcefile = "/Users/jordanking/Documents/data/arwiki/arwiki-20150901-pages-articles_parsed.txt"
-sourcefile = "/Users/jordanking/Documents/data/arwiki/arwiki4k.txt"
+sourcefile = "/Users/jordanking/Documents/data/arwiki/arwiki-20150901-pages-articles_parsed.txt"
+# sourcefile = "/Users/jordanking/Documents/data/arwiki/arwiki4k.txt"
 
 # task file prefix
-taskfile_prefix = "task_test_"
-
-# max words to draw from in the task
-expected_junk = 0.1
+taskfile_prefix = "similarity_task_"
 
 # percent of sentences a word must be in to be ignored as a stop word
 stopword_thresh = 0.05
@@ -39,9 +37,11 @@ synonym_percent = 0.5
 antonym_percent = 0.25
 shuffle_percent = 0.25
 
-synonym_target = task_size*task_count*(synonym_percent+(shuffle_percent/2)+expected_junk)
-antonym_target = task_size*task_count*(antonym_percent+(shuffle_percent/2)+expected_junk)
-top_n = (synonym_target + antonym_target)*2
+synonym_target = task_size*task_count*(synonym_percent+shuffle_percent)
+antonym_target = task_size*task_count*(antonym_percent+shuffle_percent)
+top_n = int((synonym_target + antonym_target)*8)
+
+logging.info("Top word count is: " + str(top_n))
 
 synonym_count = 0
 antonym_count = 0
@@ -62,77 +62,88 @@ synonym_pairs = []
 antonym_pairs = []
 
 # build word lists
-with open('synonym_task_test.txt', 'w') as task_file:
-    # task_file.write("Type,Left,Right,Similarity\n")
-
+with mada.Madamira() as madamira:
     while current_word < top_n:
 
-        if len(norm.normalize(word_list[current_word], ar_only=True, digits=True, alif=False, hamza=False, yaa=False, tashkil=False).strip('#')) > 0:
+        query = norm.normalize(word_list[current_word], ar_only=True, digits=True, alif=False, hamza=False, yaa=False, tashkil=False).strip('#')
+
+        if len(query) > 0:
 
             if synonym_count < synonym_target:
-                lookups = thes.thesaurus(word_list[current_word].encode('utf-8'), 'syn', ngram=1, ar=True, target_result_count=1)
+                lookups = thes.thesaurus(query.encode('utf-8'), 'syn', ngram=1, ar=True, target_result_count=1)
                 if 'syn' in lookups:
                     if len(lookups['syn']) > 0:
                         candidate = norm.normalize(lookups['syn'][0], ar_only=True, digits=True, alif=False, hamza=False, yaa=False, tashkil=False).strip('#').strip()
                         if len(candidate) > 1:
-                            if word_list[current_word] != candidate:
-                                # task_file.write('syn, ')
-                                # task_file.write(word_list[current_word].encode('utf-8') + ', ')
-                                # task_file.write(candidate.encode('utf-8')+', \n')
-                                logging.info("Found syn number:"+ str(synonym_count))
-                                synonym_pairs.append([word_list[current_word],candidate])
+
+                            # make sure the normalized and lemma forms are not the same
+                            
+                            query_lemma = madamira.process_sentence(query.encode('utf-8'))[0].lemma()
+                            candidate_lemma = madamira.process_sentence(candidate.encode('utf-8'))[0].lemma()
+                            query_norm = norm.normalize(query, alif=True, hamza=True, yaa=True, tashkil=True)
+                            candidate_norm = norm.normalize(candidate, alif=True, hamza=True, yaa=True, tashkil=True)
+                            
+                            if query_norm != candidate_norm and query_lemma != candidate_lemma:
+                                
+                                logging.info("Found syn number:" + str(synonym_count))
+                                synonym_pairs.append(['syn',query,candidate])
                                 synonym_count += 1
 
             elif antonym_count < antonym_target:
-                lookups = thes.thesaurus(word_list[current_word].encode('utf-8'), 'ant', ngram=1, ar=True, target_result_count=1) 
+                lookups = thes.thesaurus(query.encode('utf-8'), 'ant', ngram=1, ar=True, target_result_count=1) 
                 if 'ant' in lookups:
                     if len(lookups['ant']) > 0:
                         candidate = norm.normalize(lookups['ant'][0], ar_only=True, digits=True, alif=False, hamza=False, yaa=False, tashkil=False).strip('#').strip()
                         if len(candidate) > 1:
-                            if word_list[current_word] != candidate:
-                                # task_file.write('ant, ')
-                                # task_file.write(word_list[current_word].encode('utf-8') + ', ')
-                                # task_file.write(candidate.encode('utf-8')+', \n')
-                                logging.info("Found ant number:", str(antonym_count))
-                                antonym_pairs.append([word_list[current_word], candidate])
+
+                            # make sure the normalized and lemma forms are not the same
+                            query_norm = norm.normalize(query, alif=True, hamza=True, yaa=True, tashkil=True)
+                            candidate_norm = norm.normalize(candidate, alif=True, hamza=True, yaa=True, tashkil=True)
+                            query_lemma = madamira.process_sentence(query.encode('utf-8'))[0].lemma()
+                            candidate_lemma = madamira.process_sentence(candidate.encode('utf-8'))[0].lemma()
+
+                            if query_norm != candidate_norm and query_lemma != candidate_lemma:
+                                
+                                logging.info("Found ant number:"+str(antonym_count))
+                                antonym_pairs.append(['ant',query, candidate])
                                 antonym_count += 1
             else:
-                # we have all our words!
+                logging.info("Obtained all word pairs!")
                 break
 
         current_word += 1
 
-    if antonym_count < antonym_target or synonym_count < synonym_target:
-        logging.info("Ran out of words to draw from")
-    else:
-        logging.info("Used " + str(current_word) + " of the most frequent words.")
-        logging.info("Done!")
+if antonym_count < antonym_target or synonym_count < synonym_target:
+    logging.info("Ran out of words to draw from!")
+else:
+    logging.info("Used " + str(current_word) + " of the most frequent words.")
 
 # shuffle and split
 current_synonym = 0
 current_antonym = 0
+tasks = []
 for t in range(task_count):
-    with open(taskfile_prefix+str(t), 'w') as task_file:
-        for i in range(task_size*synonym_percent):
-            task_file.write(synonym_pairs[current_synonym][0].encode('utf-8')+', '+synonym_pairs[current_synonym][1].encode('utf-8')+'\n')
-            current_synonym += 1
-        for i in range(task_size*antonym_percent):
-            task_file.write(antonym_pairs[current_antonym][0].encode('utf-8')+', '+antonym_pairs[current_antonym][1].encode('utf-8')+'\n')
-            current_antonym += 1
-        for i in range(task_size*shuffled_percent/2):
-            task_file.write(antonym_pairs[current_antonym][0].encode('utf-8')+', '+synonym_pairs[current_synonym][1].encode('utf-8')+'\n')
-            task_file.write(synonym_pairs[current_synonym+1][0].encode('utf-8')+', '+antonym_pairs[current_antonym][1].encode('utf-8')+'\n')
+    task = []
+    for i in range(int(task_size*synonym_percent)):
+        task.append(synonym_pairs[current_synonym])
+        current_synonym += 1
+    for i in range(int(task_size*antonym_percent)):
+        task.append(antonym_pairs[current_antonym])
+        current_antonym += 1
+    for i in range(int(task_size*shuffle_percent)):
+        task.append(['shuf',antonym_pairs[current_antonym][1],synonym_pairs[current_synonym][2]])
+        current_antonym += 1
+        current_synonym += 1
 
-            current_antonym += 1
-            current_synonym += 1
+    # shuffle
+    random.shuffle(task)
+    tasks.append(task)
 
+# save
+for t in range(task_count):
+    task = tasks[t]
+    with open(taskfile_prefix+str(t)+".txt", 'w') as task_file:
+        for pair in task:
+            task_file.write(pair[0].encode('utf-8')+', '+pair[1].encode('utf-8')+', '+pair[2].encode('utf-8')+'\n')
 
-# save to files
-# with open('synonym_task_test.txt', 'w') as task_file:
-#     # task_file.write("Type,Left,Right,Similarity\n")
-#     for i in range(len(lhs)):
-
-#         # task_file.write('hidden, ')
-#         task_file.write(lhs[i].encode('utf-8') + ', ')
-#         task_file.write(rhs[i].encode('utf-8')+', \n')
 
