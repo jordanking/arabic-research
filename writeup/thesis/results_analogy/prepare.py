e7551a13ea99dab7a271c68eae53d046c1eb816a
Jordan King <jwk67@georgetown.edu>
2016-04-18 13:53:31 -0400

    Pre-Singh edits
    added diagrams, cleaned up table results

--- writeup/thesis/results_analogy/prepare.py
+++ writeup/thesis/results_analogy/prepare.py
@@ -41,25 +41,38 @@ for file in files:
     for index, row in df.iterrows():
         params = parseParameters(row.at['Embedding File'])
 
+        # rename control to base
+        if params['preprocessing'] == 'control':
+            params['preprocessing'] = 'base'
+
         df.set_value(index, 'wind', int(params['wind']))
         df.set_value(index, 'size', int(params['size']))
         df.set_value(index, 'mod', int(params['mod']))
         df.set_value(index, 'dig', params['dig'])
         df.set_value(index, 'tash', params['tash'])
         df.set_value(index, 'preprocessing', params['preprocessing'])
-
+        df.set_value(index, 'preprocessing_abbr', params['preprocessing'][0].upper())
+    df['rank'] = df['Scores'].rank(ascending=False).astype(int)
     df['wind'] = df['wind'].astype(int)
     df['size'] = df['size'].astype(int)
     df['mod'] = df['mod'].astype(int)
 
-    plot = df.boxplot(column='Scores', by=['size','preprocessing'])
+    plot = df.boxplot(column='Scores', by=['size','preprocessing_abbr'])
+    plt.title("Grouped by Vector Size and Preprocessing Method")
+    plt.xlabel('Size,Preprocessing (B: Base, L: Lemmas, T: Tokens)')
+    plt.annotate('B: Base\n L: Lemmas\n T: Tokens', xy=(-12, -12), xycoords='axes points',
+            size=14, ha='right', va='top',
+            bbox=dict(boxstyle='round', fc='w'))
+    plt.suptitle("")
     # plt.show()
     plt.savefig(file[:-4] + '_plot.png')
 
+
     df = df.sort_values(by=sort_metric, ascending=False)
     df = df.round({'Hit_Percent':4, 'Scores':4})
-
+    df = df.drop('preprocessing_abbr', 1)
     df = df.head(10)
+
     df.to_csv(file[:-4]+'_prepared' + '.csv', index=False)
   
     
--- writeup/thesis/results_spearman/prepare.py
+++ writeup/thesis/results_spearman/prepare.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import matplotlib.pyplot as plt
+from scipy import stats
 
 
 sort_metric = 'Spearman'
@@ -40,11 +41,18 @@ def parseParameters(filename):
         filename = filename[len(value):]
     return params
 
+indices = []
+ranking = []
+
 for file in files:
     df = pd.read_csv(file)
   
     for index, row in df.iterrows():
         params = parseParameters(row.at['Embedding File'])
+
+        # rename control to base
+        if params['preprocessing'] == 'control':
+            params['preprocessing'] = 'base'
 
         df.set_value(index, 'wind', int(params['wind'])) # slight; slight
         df.set_value(index, 'size', int(params['size'])) # no; yes
@@ -53,32 +61,59 @@ for file in files:
         df.set_value(index, 'tash', params['tash']) # no; no
         df.set_value(index, 'preprocessing', params['preprocessing']) # yes; yes
         df.set_value(index, 'preprocessing_abbr', params['preprocessing'][0].upper())
+    df['rank'] = df['Spearman'].rank(ascending=False).astype(int)
+    if 'ws353' not in file:
+        ranking.append(df['rank'])
+    else:
+        print(file)
+
     df['wind'] = df['wind'].astype(int)
     df['size'] = df['size'].astype(int)
     df['mod'] = df['mod'].astype(int)
+    df['index'] = df.index.astype(int)
 
-    # plot = df.boxplot(column='MSE', by=['size','mod','preprocessing'])
-    # plt.show()
-    # plt.savefig(file[:-4] + '_spearplot.png')
-    # labels=['4,C','4,L','4,T','7,C','7,L','7,T']
+
     plot = df.boxplot(column='Spearman', by=['wind','preprocessing_abbr'])
     plt.title("Grouped by Window and Preprocessing Method")
-    plt.xlabel('Window,Preprocessing (C: Control L: Lemmas T: Tokens)')
-    plt.annotate('C: Control L: Lemmas T: Tokens', xy=(-12, -12), xycoords='axes points',
+    plt.xlabel('Window,Preprocessing (B: Base, L: Lemmas, T: Tokens)')
+    plt.annotate('B: Base\n L: Lemmas\n T: Tokens', xy=(-12, -12), xycoords='axes points',
             size=14, ha='right', va='top',
             bbox=dict(boxstyle='round', fc='w'))
-    # plt.legend([void,void,void],['C: Control','L: Lemmas', 'T: Tokens'], loc='best')
     plt.suptitle("")
     # plt.show()
     plt.savefig(file[:-4] + '_spearplot.png')
 
     df = df.sort_values(by=sort_metric, ascending=False)
+    # print(df['rank'])
+
+    if 'ws353' not in file:
+        indices.append(df.index)
+
     df = df.round({'Accuracy':4, 'MSE':4, 'Correlation':4, 'Spearman':4})
-
     df = df.drop('preprocessing_abbr', 1)
-
     df = df.head(10)
 
     df.to_csv(file[:-4]+'_prepared' + '.csv', index=False)
 
+# print(indices)
+
+# tau, pval = stats.kendalltau(indices[0], indices[1])
+# print('multi/full: tau: {0}, p_value: {1}'.format(tau, pval))
+
+# tau, pval = stats.kendalltau(indices[0], indices[2])
+# print('multi/4: tau: {0}, p_value: {1}'.format(tau, pval))
+
+# tau, pval = stats.kendalltau(indices[1], indices[2])
+# print('full/4: tau: {0}, p_value: {1}'.format(tau, pval))
+
+# print(ranking)
+
+tau, pval = stats.kendalltau(ranking[1], ranking[2])
+print('full/4: tau: {0}, p_value: {1}'.format(tau, pval))
+
+tau, pval = stats.kendalltau(ranking[0], ranking[1])
+print('multi/full: tau: {0}, p_value: {1}'.format(tau, pval))
+
+tau, pval = stats.kendalltau(ranking[0], ranking[2])
+print('multi/4: tau: {0}, p_value: {1}'.format(tau, pval))
 
